c6bc28e907f437a87dde8c56bbb2be21ebfe3b8c
Lucas Scharenbroich <lscharen@prowestgis.com>
2021-08-05 20:24:42 -0500

    Some Tiled project files for testing

---
+++ assets/tiled/Overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Overworld" tilewidth="8" tileheight="8" tilecount="1000" columns="40">
+ <image source="../tilesets/smb.png" width="320" height="200"/>
+</tileset>
