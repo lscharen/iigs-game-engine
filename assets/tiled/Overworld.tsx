191094e7e6c0688fe7dc9f04262acb83dfaf0669
Lucas Scharenbroich <lscharen@prowestgis.com>
2021-08-14 20:56:19 -0500

    Update Tiled project to include a second layer, transparent color on tiles and better names

--- assets/tiled/Overworld.tsx
+++ assets/tiled/Overworld.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="Overworld" tilewidth="8" tileheight="8" tilecount="1000" columns="40">
- <image source="../tilesets/smb.png" width="320" height="200"/>
+<tileset version="1.5" tiledversion="1.7.2" name="App.TileSet" tilewidth="8" tileheight="8" tilecount="1000" columns="40">
+ <transformations hflip="1" vflip="1" rotate="0" preferuntransformed="0"/>
+ <image source="../tilesets/smb-16.png" trans="6b8cff" width="320" height="200"/>
 </tileset>
