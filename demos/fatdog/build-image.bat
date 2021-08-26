dc8697af3090f89440624156271bf35dec3811bf
Lucas Scharenbroich <lscharen@prowestgis.com>
2021-08-26 16:46:01 -0500

    Add separate folder for different demos

--- demos/fatdog/build-image.bat
+++ demos/fatdog/build-image.bat
@@ -6,7 +6,7 @@ REM Pass the path of the Cadius tool as the first argument (%1)
 
 set CADIUS="%1"
 set IMAGE="..\\..\\emu\\Target.2mg"
-set FOLDER="/GTEDEV/Build"
+set FOLDER="/GTEDEV/Fatdog"
 
 REM Cadius does not overwrite files, so clear the root folder first
 %CADIUS% DELETEFOLDER %IMAGE% %FOLDER%
