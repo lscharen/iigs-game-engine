2ef67e0a1c78d5b7d7c73790aa34165191051ca3
Lucas Scharenbroich <lscharen@prowestgis.com>
2021-09-02 16:53:21 -0500

    Sample project to demonstrate fringe + base tiles

---
+++ demos/fatdog-rpg/assets/world-tiles.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="world-tiles" tilewidth="8" tileheight="8" tilecount="1000" columns="40" objectalignment="topleft">
+ <transformations hflip="1" vflip="1" rotate="0" preferuntransformed="0"/>
+ <image source="world-tileset.png" trans="ff00f9" width="320" height="200"/>
+</tileset>
