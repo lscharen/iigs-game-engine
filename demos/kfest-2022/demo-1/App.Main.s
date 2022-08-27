700a3124fd5104f4b2fc8bac089a640946cd20ae
Lucas Scharenbroich <lscharen@prowestgis.com>
2022-08-27 15:53:48 -0500

    Fix demo-1 init

--- demos/kfest-2022/demo-1/App.Main.s
+++ demos/kfest-2022/demo-1/App.Main.s
@@ -47,7 +47,7 @@ appTmp0         equ   28
 
                 _MTStartUp                    ; GTE requires the miscellaneous toolset to be running
 
-                lda   #0                      ; Engine in Fast Mode
+                lda   #ENGINE_MODE_USER_TOOL  ; Engine in Fast Mode as a User Tool
                 jsr   GTEStartUp              ; Load and install the GTE User Tool
 
 ; Initialize local variables
