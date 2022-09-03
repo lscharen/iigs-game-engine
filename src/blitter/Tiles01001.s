217a1176a93d917ec8c4dceb67923bc16e5947b1
Lucas Scharenbroich <lscharen@prowestgis.com>
2022-09-02 20:37:26 -0500

    Remove obsolete blitter routines

--- src/blitter/Tiles01001.s
+++
@@ -1,6 +0,0 @@
-; _TBPriorityDynamicTile
-;
-; The priority bit allows the tile to be rendered in front of sprites.  If there's no sprite
-; in this tile area, then just fallback to the Tile00001.s implementation
-_TBPriorityDynamicTile   dw      _TBDynamicTile_00,_TBDynamicTile_00,_TBDynamicTile_00,_TBDynamicTile_00
-                         dw      _TBDynamicTile_00,_TBDynamicTile_00,_TBDynamicTile_00,_TBDynamicTile_00
