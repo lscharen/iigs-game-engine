48bf7145d87169e3a8b76784091453ba2e2aa4f5
Lucas Scharenbroich <lscharen@prowestgis.com>
2023-01-02 20:40:09 -0600

    Fix parameters to startup and tile init

--- demos/kfest-2022/demo-5/App.Main.s
+++ demos/kfest-2022/demo-5/App.Main.s
@@ -59,8 +59,9 @@ SpriteCount     equ   50
 
                 _MTStartUp                    ; GTE requires the miscellaneous toolset to be running
 
-                lda   #0                      ; Engine in Fast Mode
+                lda   #ENGINE_MODE_USER_TOOL  ; Engine in Fast Mode
                 jsr   GTEStartUp              ; Load and install the GTE User Tool
+                
                 jsr   SoundStartUp
                 jsr   StartMusic
 
@@ -81,6 +82,8 @@ SpriteCount     equ   50
 
 ; Load a tileset
 
+                pea   #0
+                pea   #512
                 pea   #^tiledata
                 pea   #tiledata
                 _GTELoadTileSet
