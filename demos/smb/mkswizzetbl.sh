552f32f917c75a926e9df574e7f205a63f84c637
Lucas Scharenbroich <lscharen@prowestgis.com>
2023-06-03 09:07:49 -0500

    Add more swizzle tables

--- demos/smb/mkswizzetbl.sh
+++ demos/smb/mkswizzetbl.sh
@@ -17,3 +17,23 @@ node swizzle.js AT2_S0 15 10 11
 node swizzle.js AT2_S1 6 12 9
 node swizzle.js AT2_S2 13 7 10
 node swizzle.js AT2_S3 14 5 6
+
+# Area Type $03, Castle
+node swizzle.js AT3_T0 2 3 4
+node swizzle.js AT3_T1 2 3 4
+node swizzle.js AT3_T2 2 5 4
+node swizzle.js AT3_T3 1 6 4
+node swizzle.js AT3_S0 15 7 14
+node swizzle.js AT3_S1 8 9 6
+node swizzle.js AT3_S2 5 2 7
+node swizzle.js AT3_S3 10 2 3
+
+# Area Type $00, Water
+node swizzle.js ATx_T0 2 3 4
+node swizzle.js ATx_T1 5 6 7
+node swizzle.js ATx_T2 8 9 7
+node swizzle.js ATx_T3 1 9 7
+node swizzle.js ATx_S0 15 10 14
+node swizzle.js ATx_S1 11 8 10
+node swizzle.js ATx_S2 12 8 10
+node swizzle.js ATx_S3 7 8 11
--- demos/smb/palette.s
+++ demos/smb/palette.s
@@ -75,7 +75,7 @@ nesPalette
 ;
 ; The rest are remapped.
 ;
-; Underground:
+; Underground  (AreaType = $02)
 ;
 ; T0: $0F $29 $1A $09
 ; T1: --- $3C $1C $0F
@@ -98,7 +98,7 @@ nesPalette
 ; S2: 0 9 7 A
 ; S3: 0 E D 6
 ;
-; Above Ground
+; Above Ground  (AreaType = $01)
 ;
 ; T0: $22 $29 $1A $0F
 ; T1: --- $36 $17 $0F
@@ -119,3 +119,49 @@ nesPalette
 ; S1: 0 C 7 A
 ; S2: 0 9 7 A
 ; S3: 0 4 5 6
+;
+; Castle (AreaType = $00)
+;
+; Bowser changes S1 palette when he loads
+;
+; T0: $0F $30 $10 $00
+; T1: --- $30 $10 $00
+; T2: --- $30 $16 $00
+; T3: --- RRR $17 $00
+; S0: --- SS1 $27 SS2 
+; S1: --- $1C $36 $17
+; S2: --- $16 $30 $27
+; S3: --- $1D $30 $10 --> $0F  RR $30 $10  $00 $16 $17 $27  $1C $36 $1D ---  --- --- SS1 SS2 : 2 free colors
+;                     --> $00 $01 $02 $03  $04 $05 $06 $07  $08 $09 $0A $0B  $0C $0D $0E $0F
+; Mapped palettes
+;
+; T0: 0 2 3 4
+; T1: 0 2 3 4
+; T2: 0 2 5 4
+; T3: 0 1 6 4
+; S0: 0 F 7 E
+; S1: 0 8 9 6
+; S2: 0 5 2 7
+; S3: 0 10 2 3
+;
+; Water
+;
+; T0: BBB $15 $12 $25
+; T1: --- $3A $1A $0F
+; T2: --- $30 $12 $0F
+; T3: --- RRR $12 $0F
+; S0: --- SS1 $27 SS2 
+; S1: --- $10 $30 $27
+; S2: --- $16 $30 $27
+; S3: --- $0F $30 $10 --> BBB RRR $15 $12  $25 $3A $1A $0F  $30 $12 $27 $10  $16 --- SS1 SS2 : 1 free colors
+;                     --> $00 $01 $02 $03  $04 $05 $06 $07  $08 $09 $0A $0B  $0C $0D $0E $0F
+; Mapped palettes
+;
+; T0: 0 2 3 4
+; T1: 0 5 6 7
+; T2: 0 8 9 7
+; T3: 0 1 9 7
+; S0: 0 F A E
+; S1: 0 B 8 A
+; S2: 0 C 8 A
+; S3: 0 7 8 B
